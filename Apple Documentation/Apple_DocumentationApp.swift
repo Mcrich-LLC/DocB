a3489b5cae2eb2de64ff5d42d61f2fa7faa0d840
Morris Richman <81453549+Mcrich23@users.noreply.github.com>
2024-10-05 20:37:22 -0700

    Initial Commit

---
+++ Apple Documentation/Apple_DocumentationApp.swift
@@ -0,0 +1,17 @@
+//
+//  Apple_DocumentationApp.swift
+//  Apple Documentation
+//
+//  Created by Morris Richman on 10/5/24.
+//
+
+import SwiftUI
+
+@main
+struct Apple_DocumentationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

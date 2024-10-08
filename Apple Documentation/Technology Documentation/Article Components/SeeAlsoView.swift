76ab890832c2f593e6f559a43924803012270a69
Morris Richman <81453549+Mcrich23@users.noreply.github.com>
2024-10-08 02:32:18 -0700

    added look parity to website for SeeAlso

--- Apple Documentation/Technology Documentation/Article Components/SeeAlsoView.swift
+++ Apple Documentation/Technology Documentation/Article Components/SeeAlsoView.swift
@@ -24,8 +24,17 @@ struct SeeAlsoView: View {
                             if let reference = article.references[identifier], let title = reference.title {
                                 NavigationLink(value: reference) {
                                     GroupBox {
-                                        Text(title)
-                                            .frame(maxWidth: .infinity, alignment: .leading)
+                                        VStack {
+                                            Text(getFullTitle(reference))
+                                                .frame(maxWidth: .infinity, alignment: .leading)
+                                            
+                                            if let abstract = reference.abstract {
+                                                AbstractView(abstract: abstract)
+                                                    .frame(maxWidth: .infinity, alignment: .leading)
+                                                    .foregroundStyle(Color.primary)
+                                            }
+                                        }
+                                        .multilineTextAlignment(.leading)
                                     }
                                 }
                             }
@@ -35,5 +44,21 @@ struct SeeAlsoView: View {
                 }
             }
         }
+    }
+    
+    func getFullTitle(_ reference: Reference) -> AttributedString {
+        
+        var nsAttributedString: NSMutableAttributedString = .init()
+        
+        let fragments: String = (reference.fragments ?? []).dropLast().map({ $0.text }).joined()
+        let fragmentAttributes: [NSAttributedString.Key: Any] = [.foregroundColor: UIColor.secondaryLabel]
+        let fragmentAttributedString = NSAttributedString(string: fragments, attributes: fragmentAttributes)
+        nsAttributedString.append(fragmentAttributedString)
+        
+        let titleAttributes: [NSAttributedString.Key: Any] = [.foregroundColor: UIColor.accent]
+        let titleAttributedString = NSAttributedString(string: reference.title ?? "", attributes: titleAttributes)
+        nsAttributedString.append(titleAttributedString)
+        
+        return AttributedString(nsAttributedString)
     }
 }

2cc5ad5f69acd67139a337777fd0b67917089a3a
Morris Richman <81453549+Mcrich23@users.noreply.github.com>
2024-10-08 03:05:51 -0700

    Update DetailsView.swift

--- Apple Documentation/Technology Documentation/Article Components/DetailsView.swift
+++ Apple Documentation/Technology Documentation/Article Components/DetailsView.swift
@@ -11,24 +11,26 @@ struct DetailsView: View {
     let details: ContentSection.Details
     
     var body: some View {
-        VStack {
+        VStack(spacing: 20) {
             Text("Details")
                 .font(.title3)
                 .bold()
                 .frame(maxWidth: .infinity, alignment: .leading)
             
-            VStack(spacing: 20) {
-                VStack {
-                    Text("Name")
-                        .bold()
-                    Text(details.name.capitalized)
-                }
-                VStack {
-                    Text("Type")
-                        .bold()
-                    ForEach(details.value, id: \.baseType) { value in
-                        Text(value.baseType.capitalized)
-                    }
+            VStack {
+                Text("Name")
+                    .bold()
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                Text(details.name)
+                    .frame(maxWidth: .infinity, alignment: .leading)
+            }
+            VStack {
+                Text("Type")
+                    .bold()
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                ForEach(details.value, id: \.baseType) { value in
+                    Text(value.baseType.capitalized)
+                        .frame(maxWidth: .infinity, alignment: .leading)
                 }
             }
         }

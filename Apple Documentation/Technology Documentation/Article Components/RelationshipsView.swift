97c6d67fda416a5623312d1767215ad87939b681
Morris Richman <81453549+Mcrich23@users.noreply.github.com>
2024-10-10 21:22:47 -0700

    moved relationshipsview to reference LinksGridListView

--- Apple Documentation/Technology Documentation/Article Components/RelationshipsView.swift
+++ Apple Documentation/Technology Documentation/Article Components/RelationshipsView.swift
@@ -20,53 +20,11 @@ struct RelationshipsView: View {
                 
                 ForEach(relationshipsSections) { section in
                     Section(header: Text(section.title)) {
-                        ForEach(section.identifiers, id: \.self) { identifier in
-                            if let reference = article.references[identifier], reference.title != nil {
-                                ReferenceNavigationLinkButton(reference: reference) {
-                                    GroupBox {
-                                        VStack {
-                                            Text(getFullTitle(reference))
-                                                .frame(maxWidth: .infinity, alignment: .leading)
-                                            
-                                            if let abstract = reference.abstract {
-                                                AbstractView(abstract: abstract)
-                                                    .frame(maxWidth: .infinity, alignment: .leading)
-                                                    .foregroundStyle(Color.primary)
-                                            }
-                                        }
-                                        .multilineTextAlignment(.leading)
-                                    }
-                                }
-                            }
-                        }
+                        LinksGridListView(identifiers: section.identifiers, style: .list, article: article)
                     }
                     .frame(maxWidth: .infinity, alignment: .leading)
                 }
             }
         }
-    }
-    
-    func getFullTitle(_ reference: Reference) -> AttributedString {
-        
-        let nsAttributedString: NSMutableAttributedString = .init()
-        
-        let fragments: String = (reference.fragments ?? []).dropLast().map({ $0.text }).joined()
-        let fragmentAttributes: [NSAttributedString.Key: Any] = [.foregroundColor: UIColor.secondaryLabel]
-        let fragmentAttributedString = NSAttributedString(string: fragments, attributes: fragmentAttributes)
-        nsAttributedString.append(fragmentAttributedString)
-        
-        if !nsAttributedString.string.contains(reference.title ?? "") {
-            let titleAttributes: [NSAttributedString.Key: Any] = [.foregroundColor: UIColor.accent]
-            let titleAttributedString = NSAttributedString(string: reference.title ?? "", attributes: titleAttributes)
-            nsAttributedString.append(titleAttributedString)
-        } else {
-            let string = nsAttributedString.string
-            let range: NSRange = string.range(of: reference.title ?? "")!.nsRange(in: string)
-            
-            let titleAttributes: [NSAttributedString.Key: Any] = [.foregroundColor: UIColor.accent]
-            nsAttributedString.addAttributes(titleAttributes, range: range)
-        }
-        
-        return AttributedString(nsAttributedString)
     }
 }

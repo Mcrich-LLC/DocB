63c0bfc0bf2746f12e0879d7e3e69d3239cf1688
Morris Richman <81453549+Mcrich23@users.noreply.github.com>
2026-04-25 23:13:46 -0700

    add visionos 2 support to package

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "DocBCore",
-    platforms: [.macOS(.v15), .iOS(.v18)],
+    platforms: [.macOS(.v15), .iOS(.v18), .visionOS(.v2)],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(

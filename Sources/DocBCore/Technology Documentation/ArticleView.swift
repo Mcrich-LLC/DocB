5a6dad35bf4a2542076a9825ab3fc31356843cc1
Morris Richman <81453549+Mcrich23@users.noreply.github.com>
2026-05-27 01:17:57 -0700

    update accent color for article view

--- Sources/DocBCore/Technology Documentation/ArticleView.swift
+++ Sources/DocBCore/Technology Documentation/ArticleView.swift
@@ -28,14 +28,16 @@ struct ArticleView: View {
     
     /// The general accent color.
     private var accentColor: Color? {
-        let color = article?.metadata.color?.standardColorIdentifier.swiftUIColor ?? article?.metadata.role.accentColor
+        if let standardColorIdentifier = article?.metadata.color?.standardColorIdentifier {
+            return standardColorIdentifier.readableAccentColor
+        }
         
         // Keep contrast good
-        guard color != .gray && color != .secondary && (article?.metadata.role != .article || article?.metadata.color?.standardColorIdentifier != nil) else {
+        guard article?.metadata.role != .article else {
             return nil
         }
         
-        return color
+        return article?.metadata.role.readableAccentColor
     }
     
     var body: some View {
